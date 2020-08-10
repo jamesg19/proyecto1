018ca898f20eb7b2c1e8da48e8944535ea97471f
james <james@PC-HOME>
2020-08-10 12:56:55 -0400

    JFRAME

--- tiendaIPC2/src/main/java/gc/tiendaipc2/Main.java
+++ tiendaIPC2/src/main/java/gc/tiendaipc2/Main.java
@@ -5,6 +5,8 @@
  */
 package gc.tiendaipc2;
 
+import gc.tiendaipc2.FRAME.MENU;
+
 /**
  *
  * @author james
@@ -16,6 +18,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        MENU Menu=new MENU();
+        Menu.setVisible(true);
     }
     
 }
